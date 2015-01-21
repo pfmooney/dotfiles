ac113801750a4201c41f3700bd232746cf6913b3
Patrick Mooney <patrick.f.mooney@gmail.com>
2015-01-20 19:29:08 -0600

    scripts: Add helper to fix steam runtime issues

---
+++ scripts/fixsteam.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+# Copyright (C) 2013-2014 Michael Gilbert <mgilbert@debian.org>
+# License: MIT
+
+config=$HOME/.steam
+ubuntu32=$config/ubuntu12_32
+
+# remove steam-runtime's libstdc++, which is incompatible with newer mesa drivers
+# (https://bugs.freedesktop.org/78242)
+rm -f $ubuntu32/steam-runtime/i386/usr/lib/i386-linux-gnu/libstdc++.so.6*
+rm -f $ubuntu32/steam-runtime/amd64/usr/lib/x86_64-linux-gnu/libstdc++.so.6*
