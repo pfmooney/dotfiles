b04efadb4d63c00657fad11b14e04a4061f00357
Patrick Mooney <pmooney@pfmooney.com>
2023-04-02 16:19:26 -0500

    neovim: Switch to Lazy package management

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,13 +1,89 @@
+-- load lazy.nvim plugin manager
+vim.opt.rtp:prepend(vim.fn.stdpath('config') .. '/lazy.nvim')
+
+-- Set the leader early, before other plugins are loaded
+vim.g.mapleader = ","
+
+require('lazy').setup({
+  {
+    'pfmooney/vim-pmolokai',
+    commit = 'c3a0477fd7ac91bd9a4983ba1f1f472315c05a10',
+    config = function()
+      -- Set colorscheme once it's loaded
+      vim.cmd([[colorscheme pmolokai]])
+    end,
+  },
+  {
+    'itchyny/lightline.vim',
+    commit = 'b1e91b41f5028d65fa3d31a425ff21591d5d957f',
+    config = function()
+      -- always show status
+      vim.opt.laststatus=2
+      -- See statusline for lineinfo documentation
+      vim.g.lightline = {
+        colorscheme = "one",
+        component = {
+          lineinfo = "%3l:%-5(%c%V%)"
+        },
+        mode_map = {
+          n = "NORM",
+          i = "INS ",
+          R = "REP ",
+          v = "VIS ",
+          V = "V-L ",
+          ["<C-v>"] = "V-B ",
+          c = "COMM",
+          s = "SEL ",
+          S = "S-L ",
+          ["<C-s>"] = "S-B ",
+          t = "TERM",
+        }
+      }
+    end,
+  },
+
+  -- Utilities
+  {
+    'tpope/vim-fugitive',
+    commit = '96c1009fcf8ce60161cc938d149dd5a66d570756',
+  },
+  {
+    'tpope/vim-commentary',
+    commit = 'f8238d70f873969fb41bf6a6b07ca63a4c0b82b1',
+  },
+
+  -- LSP
+  {
+    'neovim/nvim-lspconfig',
+    commit = '443b6a8f87ef52ac252b982d9bd1afb8092e3e24',
+  },
+  {
+    'j-hui/fidget.nvim',
+    commit = '0ba1e16d07627532b6cae915cc992ecac249fb97',
+  },
+
+  -- Telescope (and more)
+  {
+    'nvim-lua/plenary.nvim',
+    commit = '253d34830709d690f013daf2853a9d21ad7accab',
+  },
+  {
+    'nvim-lua/popup.nvim',
+    commit = 'b7404d35d5d3548a82149238289fa71f7f6de4ac',
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    commit = '942fe5faef47b21241e970551eba407bc10d9547',
+  },
+})
+
+
 local fn = vim.fn -- to call Vim functions e.g. fn.bufnr()
 local g = vim.g -- a table to access global variables
 local opt = vim.opt -- to set options
 local u = require('utils')
 
-g.mapleader = ","
-
-vim.cmd [[colorscheme pmolokai]]
 vim.o.bg = "dark"
-
 
 vim.wo.list = true
 vim.wo.number = true
@@ -15,29 +91,6 @@ vim.o.listchars = "tab:>-"
 vim.o.fillchars = "fold: "
 -- Show only the menu, not the preview (in a scratch window)
 vim.o.completeopt = "menu"
-
--- always show
-opt.laststatus=2
--- See statusline for lineinfo documentation
-g.lightline = {
-  colorscheme = "one",
-  component = {
-    lineinfo = "%3l:%-5(%c%V%)"
-  },
-  mode_map = {
-    n = "NORM",
-    i = "INS ",
-    R = "REP ",
-    v = "VIS ",
-    V = "V-L ",
-    ["<C-v>"] = "V-B ",
-    c = "COMM",
-    s = "SEL ",
-    S = "S-L ",
-    ["<C-s>"] = "S-B ",
-    t = "TERM",
-  }
-}
 
 -- Return to same line in file
 function file_line_return()
